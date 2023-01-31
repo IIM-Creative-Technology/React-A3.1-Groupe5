525f652cbed7b31e72461edafb3446df89340060
Cedric <cedricjamme78@gmail.com>
2023-01-31 15:43:24 +0100

    add router + harry potter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./component/Header";
+import HarryCard from "./component/HarryCard";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return (<div>
+    <Routes>
+      <Route path="/" element = {<Header/>}/>
+      <Route path="/harry" element = {<HarryCard/>}/>
+    </Routes>
+    
     </div>
   );
 }
---
+++ src/component/Header.jsx
@@ -0,0 +1,12 @@
+import '../style/Header.css'
+
+function Header() {
+    const title = "Devine le Personnage"
+
+
+    return (
+        <h1>{title}</h1>
+    )
+}
+
+export default Header
